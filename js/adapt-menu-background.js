a0bac192ffb09f690ec84fbe904f44db60d03bf4
Simon  Date <simondate@yahoo.co.uk>
2017-03-08 11:58:36 +0000

    Added a menuHeader attribute too
    Allows for multiple images to be added

--- js/adapt-menu-background.js
+++ js/adapt-menu-background.js
@@ -7,16 +7,21 @@ define(["coreJS/adapt"], function(Adapt) {
 
         className: "extension",
 
-        initialize: function() {
-        }
+        initialize: function() {}
     });
 
 
     Adapt.on("menuView:ready", function() {
-        $('.menu-container-inner').css('background', 'url(' + Adapt.course.get('_menuBackground')._link + ')');
-        $('.menu-container-inner').css('padding-bottom', '15%');
-        $('.menu-container-inner').css('background-size', 'cover');
-        $('.menu-container-inner').css('background-position-y', '200px');
+        if (Adapt.course.get('_menuBackground')._menuHeaderLink != undefined) {
+            $('.menu-header').css('background', 'url(' + Adapt.course.get('_menuBackground')._menuHeaderLink + ')');
+        }
+        if (Adapt.course.get('_menuBackground')._backgroundLink != undefined) {
+            $('.menu-container-inner').css('background', 'url(' + Adapt.course.get('_menuBackground')._backgroundLink + ')');
+            $('.menu-container-inner').css('padding-bottom', '15%');
+            $('.menu-container-inner').css('background-size', 'cover');
+            $('.menu-container-inner').css('background-position-y', '200px');
+        }
+
     });
 
     return menuBackground;
