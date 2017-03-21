60a67f67b18690b65b8a514b5819e27d1fc6ea75
Louise <louise.bennett@kcl.ac.uk>
2017-03-21 13:25:52 +0000

    Set image to cover

--- js/adapt-menu-background.js
+++ js/adapt-menu-background.js
@@ -13,12 +13,14 @@ define(["coreJS/adapt"], function(Adapt) {
     Adapt.on("pageView:ready", function() {
         if (Adapt.course.get('_menuBackground')._menuHeader._applyToPage && !(Adapt.course.get('_menuBackground')._menuHeader._link === "")) {
           $('.page-header').css('background', 'url(' + Adapt.course.get('_menuBackground')._menuHeader._link + ')');
+          $('.page-header').css('background-size', 'cover');
         }
     });
     Adapt.on("menuView:ready", function() {
         console.log(Adapt.course.get('_menuBackground')._menuHeader._link);
         if (!(Adapt.course.get('_menuBackground')._menuHeader._link === "")) {
             $('.menu-header').css('background', 'url(' + Adapt.course.get('_menuBackground')._menuHeader._link + ')');
+            $('.menu-header').css('background-size', 'cover');
         }
         if (!(Adapt.course.get('_menuBackground')._backgroundLink === "")) {
             $('.menu-container-inner').css('background', 'url(' + Adapt.course.get('_menuBackground')._backgroundLink + ')');
